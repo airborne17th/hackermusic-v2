374adac5040972403a78d44d7cd382e6b6617b10
airborne17th <43795410+airborne17th@users.noreply.github.com>
2020-06-09 12:58:23 -0500

    small changes
    eventually will flesh out links

--- src/view/header.js
+++ src/view/header.js
@@ -1,28 +1,39 @@
-import React from 'react';
-import '../App.css';
-import logo from '../components/Assets/img/logo.png';
-import user from '../components/Assets/img/user.png';
+import React from "react";
+import { Link } from "react-router-dom";
+import "../App.css";
+import logo from "../components/Assets/img/logo.png";
+import user from "../components/Assets/img/user.png";
 function Header() {
-    return (
-        <div className="Header">
-        <header className="App-header">
-            <div className="Header-Container">
-            <img src={logo} height='100' width='100' alt="Hacker Music Logo"></img><br></br>
-            <span>Hacker Music</span>
-            </div>
-            <div className="Header-Container">
-            <img src={user} height='100' width='100' alt="User Profile Pic"></img><br></br>
-            <span>airborne17th</span>
-            </div>
-            <div className="Header-Container">
-            <a href="#">Home</a><br></br>
-            <a href="#">About</a><br></br> 
-            <a href="#">Profile</a><br></br>
-            <button className='button'>Log Off</button>
-            </div>
-        </header>
+  return (
+    <div className="Header">
+      <header className="App-header">
+        <div className="Header-Container">
+          <img
+            src={logo}
+            height="100"
+            width="100"
+            alt="Hacker Music Logo"
+          ></img>
+          <br></br>
+          <span>Hacker Music</span>
         </div>
-    );
-  }
+        <div className="Header-Container">
+          <img src={user} height="100" width="100" alt="User Profile Pic"></img>
+          <br></br>
+          <span>airborne17th</span>
+        </div>
+        <div className="Header-Container">
+          <a href="#">Home</a>
+          <br></br>
+          <a href="about.html">About</a>
+          <br></br>
+          <a href="#">Profile</a>
+          <br></br>
+          <button className="button">Log Off</button>
+        </div>
+      </header>
+    </div>
+  );
+}
 
 export default Header;
